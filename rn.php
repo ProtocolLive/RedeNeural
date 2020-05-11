943962556ff0535d55e13911debefeb939c748bb
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-05-11 19:07:21 -0300

    Usando ponteiro para a camada de saída

--- rn.php
+++ rn.php
@@ -11,14 +11,13 @@ class RedeNeural{
   private int $QtOcultas = 0;
   private int $QtNeuronioOculta = 0;
   private int $QtSaidas = 0;
-  private int $IdSaida = 0;
+  private array $CamadaSaida;
 
   public function __construct(int $QtEntradas, int $QtOcultas, int $QtNeuronioOculta, int $QtSaidas){
     $this->QtEntradas = $QtEntradas;
     $this->QtOcultas = $QtOcultas;
     $this->QtNeuronioOculta = $QtNeuronioOculta;
     $this->QtSaidas = $QtSaidas;
-    $this->IdSaida = $this->QtOcultas + 1;
     //Entradas
     for($i = 0; $i < $this->QtEntradas; $i++):
       $this->Rede[0][$i]['Saida'] = 1;
@@ -40,12 +39,14 @@ class RedeNeural{
       endfor;
     endfor;
     //Saidas
+    $CamadaSaida = $QtOcultas + 1;
     for($i = 0; $i < $QtSaidas; $i++):
-      $this->Rede[$this->IdSaida][$i]['Saida'] = 1;
+      $this->Rede[$CamadaSaida][$i]['Saida'] = 1;
       for($k = 0; $k < $this->QtNeuronioOculta; $k++):
-        $this->Rede[$this->IdSaida][$i]['Pesos'][$k] = rand(-1000, 1000);
+        $this->Rede[$CamadaSaida][$i]['Pesos'][$k] = rand(-1000, 1000);
       endfor;
     endfor;
+    $this->CamadaSaida = &$this->Rede[$CamadaSaida];
   }
 
   public function Entrada():bool{
@@ -56,7 +57,7 @@ class RedeNeural{
   }
 
   public function Saida($Id):int{
-    return $this->Rede[$this->IdSaida][$Id]['Saida'];
+    return $this->CamadaSaida[$Id]['Saida'];
   }
 
   public function PesosSet(array $Rede):bool{
@@ -72,7 +73,7 @@ class RedeNeural{
     $return = [];
     foreach($this->Rede as $Id1 => $Camada):
       foreach($Camada as $Id2 => $Peso):
-        if($Id1 > 0 and $Id1 < $this->IdSaida):
+        if($Id1 > 0 and $Id1 <= $this->QtOcultas):
           $return[$Id1][$Id2] = $this->Rede[$Id1][$Id2]['Pesos'];
         endif;
       endforeach;
